a10ad86a4225e31a90ab3aef8681b55b3b24b851
v_supezhang@tentcent.com <v_supezhang@tencent.com>
2022-05-11 11:44:30 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.css';
+import '@tencent/udesign/dist/styles.css';
+
+// 重构css文件
+import './refactor';
+// theme
+import './theme/_light.scss';
+import './theme/_dark.scss';
+// 变量
+import './_variable.scss';
+// 通用样式
+import './common.scss';
---
+++ refactor/index.ts
@@ -0,0 +1,6 @@
+import './css/materialdesignicons.min.css';
+import './css/reset.css';
+import './css/color.css';
+import './css/hightchart.scss';
+
+import './css/style.scss';
